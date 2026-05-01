f78d3853338e77e607e049ae43627824cec89b1f
karan6100 <karanbari1996@gmail.com>
2026-05-01 13:30:56 +0530

    Remove .gitignore from repo but keep locally

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-exec streamlit run streamlit_app.py \
-  --server.port=7860 \
-  --server.address=0.0.0.0 \
-  --server.headless=true \
-  --browser.gatherUsageStats=false
